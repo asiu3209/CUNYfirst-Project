bf0d5c3c62c1a2e24a03094a4e1619f6b481b1c0
Aaron Siu <aarons3209@gmail.com>
2025-04-03 13:08:53 -0400

    ~

--- script.js
+++ script.js
@@ -3,19 +3,22 @@ const username =
     "edison@login.cuny.edu",
     "alex@login.cuny.edu",
     "amal@login.cuny.edu"];
-const password = ["12345678",
+const password = ["1234",
     "abcdefgh",
     "87654321",
     "abcdefgh"];
 
 function login(){
-    const user = document.getElementById("username");
-    const pass = document.getElementById("password");
-    for(let i =0;i<username.length;i++){
+    const user = document.getElementById("username").value;
+    const pass = document.getElementById("password").value;
+    const errors = document.getElementsByClassName("error");
+    for(let i = 0; i < username.length; i++){
         if(user === username[i] && pass === password[i]){
-            console.log("sucessful login");//replace this with something else
+            window.location.href = "redirectPage.html";
             return;
         }
     }
-    
+    for (let error of errors) {
+        error.style.display = 'block';
+    }
 }
